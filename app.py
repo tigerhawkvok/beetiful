d4da58bb409c5409964649f788969ab8b8417de2
Philip Kahn <tigerhawkvok@gmail.com>
2026-05-16 22:43:32 -0700

    avoid my very first error I saw

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+DEFAULT_PORT = 3001
+
 # Read port from environment variable, defaulting to 3001 if not set
-port = int(os.getenv("FLASK_PORT", 3001))
+try:
+    port = int(os.getenv("FLASK_PORT", DEFAULT_PORT))
+except ValueError:
+    print("Invalid FLASK_PORT value. Using default port 3001.")
+    port = DEFAULT_PORT
 
 app.run(debug=True, host="0.0.0.0", port=port)
